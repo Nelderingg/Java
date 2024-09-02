29b420e0cae55143535fec9d7702ad6824261a1c
Nate Eldering <elderingn@vcu.edu>
2024-09-02 11:44:40 -0400

    updated variable calculator

--- variable_calculator.java
+++ variable_calculator.java
@@ -33,41 +33,75 @@ public class variable_calculator{
         // Split the instruction into parts
         String[] parts = instruction.split("\\s+");
         
-        if (parts.length != 3 && parts.length != 5) {
-            return false;
-        }
-        
         // Handle the assignment instruction
         String varnameL = parts[0];
         
-        if (variables.containsKey(varnameL)) {
-            // Variable on the left already assigned a value
-            return false;
-        }
         
         // If the instruction is of the form varnameL = intValue
-        if (parts.length == 3 && parts[1].equals("=")) {
+        if (parts.length == 3 && parts[1].equals("=")) 
+        {
                 int intValue = Integer.parseInt(parts[2]);
                 // Assign the intValue to varnameL
                 variables.put(varnameL, intValue);
                 System.out.println(intValue);
                 return true;
         }
+
+        else if (parts.length == 1)
+        {
+            String varname = parts[0];
+
+            if (variables.containsKey(varname))
+            {
+                System.out.println(variables.get(varname));
+                return true;
+            }
+            else
+            {
+                System.out.println("No value stored in the variable" + varname);
+                return true;
+            }
+        }
+
         // If the instruction is of the form varnameL = varnameR1 operation varnameR2
-        else if (parts.length == 5 && parts[1].equals("=")){
+        else if (parts.length == 5 && parts[1].equals("="))
+        {
             String varnameR1 = parts[2];
             String operation = parts[3];
             String varnameR2 = parts[4];
+            int valueR1;
+            int valueR2;
             
-            // Check if varnameR1 and varnameR2 have been assigned values
-            if (!variables.containsKey(varnameR1) || !variables.containsKey(varnameR2)) {
-                // Variables on the right not assigned values
-                return false;
+            // // Check if varnameR1 and varnameR2 have been assigned values
+            // if (!variables.containsKey(varnameR1) || !variables.containsKey(varnameR2)) {
+            //     // Variables on the right not assigned values
+            //     return false;
+            // }
+
+            if (variables.containsKey(varnameR1))
+            {
+                valueR1 = variables.get(varnameR1);
             }
+
+            else
+            {
+                valueR1 = Integer.parseInt(varnameR1);
+            }
+
+            if (variables.containsKey(varnameR2))
+            {
+                valueR2 = variables.get(varnameR2);
+            }
+
+            else
+            {
+                valueR2 = Integer.parseInt(varnameR2);
+            }
+
             
             // Get the values of varnameR1 and varnameR2
-            int valueR1 = variables.get(varnameR1);
-            int valueR2 = variables.get(varnameR2);
+            // int valueR1 = variables.get(varnameR1);
+            // int valueR2 = variables.get(varnameR2);
             
             // Perform the operation and assign the result to varnameL
             int result;
@@ -91,7 +125,9 @@ public class variable_calculator{
             variables.put(varnameL, result);
             System.out.println(result);
             return true;
-        } else {
+        }
+        else
+        {
             // Invalid instruction format
             return false;
         }
